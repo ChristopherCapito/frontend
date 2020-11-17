141041677ecb8c5e2aa27084fef0ef0bc649cdbf
Christopher Capito <christopher@capito.rocks>
2020-11-17 11:31:26 +0100

    Updated loading query screen to use the container.

--- components/query.js
+++ components/query.js
@@ -7,8 +7,8 @@ const Query = ({ children, query, id }) => {
     variables: { id },
   });
 
-  if (loading) return <p className="text-light">Loading...</p>;
-  if (error) return <p className="text-light">Error: {JSON.stringify(error)}</p>;
+  if (loading) return <p className="text-light mt-12 container mx-auto px-8">Loading...</p>;
+  if (error) return <p className="text-light mt-12 container mx-auto px-8">Error: {JSON.stringify(error)}</p>;
   return children({ data });
 };
 
