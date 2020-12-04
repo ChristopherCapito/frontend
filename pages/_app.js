290bd927c5e34fe82bc1f294eb4ee70641279922
Christopher Capito <christopher@capito.rocks>
2020-12-04 18:36:08 +0100

    Motion div seems to be the problem

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,7 @@ export default function App({ Component, pageProps, router }) {
       <ApolloProvider client={apolloClient}>
         <Nav />
         <AnimatePresence
+          exitBeforeEnter
           onExitComplete={() => {
             window.scroll({
               top: 0,
