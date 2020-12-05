35d0b97ac6a46a30ef7341d6f10ec1345729d6d6
Christopher Capito <christopher@capito.rocks>
2020-12-05 16:15:19 +0100

    Changed Mobile Nav

--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 const Nav = () => (
   <nav className="text-light">
-    <div className="py-12 mx-auto flex container px-8 justify-between items-end">
-      <div name="Name Tag" className="flex-grow  mr-6 lg:mr-0">
+    <div className="py-12 mx-auto block md:flex container px-8 justify-between items-end">
+      <div name="Name Tag" className="flex-grow  mr-6 md:mr-0">
         <Link scroll={false} href="/">
           <a>
             <span className="text-md font-bold tracking-tight">Christopher Capito</span>
@@ -12,20 +12,20 @@ const Nav = () => (
         <hr />
       </div>
       <div name="Regular Menu">
-        <div className="flex hidden lg:block lg:items-center lg:w-auto block">
+        <div className="flex block md:items-center md:w-auto block">
           <div className="text-sm">
             <Link scroll={false} href="/">
-              <a className="mt-4 lg:inline-block lg:mt-0  font-medium mr-4">Home</a>
+              <a className="mt-4 inline-block lg:mt-0  font-medium mr-4">Home</a>
             </Link>
             <Link scroll={false} href="/portfolio">
-              <a className="mt-4 lg:inline-block lg:mt-0  font-medium mr-4">Portfolio</a>
+              <a className="mt-4 inline-block lg:mt-0  font-medium mr-4">Portfolio</a>
             </Link>
             <Link scroll={false} href="/contact">
-              <a className="mt-4 lg:inline-block lg:mt-0   font-medium mr-4">Contact</a>
+              <a className="mt-4 inline-block lg:mt-0   font-medium mr-4">Contact</a>
             </Link>
           </div>
         </div>
-        <hr className="hidden lg:block" />
+        <hr className="hidden md:block" />
       </div>
     </div>
   </nav>
