45da1c4a754d1cd46ac9cb326cc30a4fa1daeac8
Christopher Capito <christopher@capito.rocks>
2020-12-23 03:42:39 +0100

    +

--- utils/formatFunctions.js
+++ utils/formatFunctions.js
@@ -3,8 +3,9 @@ export const FormatSectionNumber = (num) => (num < 10 ? `0${num}.` : `${num}.`);
 
 // Assemble the complete URL.
 export const getUrl = (input, size) => {
-  const output = [input.slice(0, 74), `${size}_`, input.slice(74)].join('');
-
-  if (input !== null) return process.env.NODE_ENV !== 'development' ? output : process.env.API_URL + input;
+  if (input !== null)
+    return process.env.NODE_ENV !== 'development'
+      ? [input.slice(0, 74), `${size}_`, input.slice(74)].join('')
+      : process.env.API_URL + input;
   // return console.warn('getURL got "null" as a URL. Nothing will be displayed');
 };
